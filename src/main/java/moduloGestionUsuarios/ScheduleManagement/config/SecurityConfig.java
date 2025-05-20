4888c538e284451885409dc897e0b8d4aaa0cb4c
Miguel <miguel.vanegas-c@mail.escuelaing.edu.co>
2025-05-19 23:23:23 -0500

    fixing getters

--- src/main/java/moduloGestionUsuarios/ScheduleManagement/config/SecurityConfig.java
+++ src/main/java/moduloGestionUsuarios/ScheduleManagement/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Configuration class for Spring Security.
@@ -36,6 +38,19 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
 
